9f506d178c0ce3988e4f738c8e50cfde3a673eb3
Andrew Sutherland <asutherland@asutherland.org>
2009-01-11 03:39:50 -0800

    poor man's autocomplete.
    --HG--
    rename : client/messages.css => client/bindings.css

---
+++ client/cloda-completers.js
@@ -0,0 +1,27 @@
+var ContactCompleter = {
+  complete: function(aAutocomplete, aText) {
+    console.log("Contact completer firing on", aText);
+    Gloda.dbContacts.view("contact_ids/by_suffix", {
+      startkey: aText,
+      endkey: aText + "\u9999",
+      include_docs: true,
+      limit: 10,
+      success: function(result) {
+        var seen = {};
+        var nodes = [];
+        result.rows.forEach(function (row) {
+          if (!(row.id in seen)) {
+            node = $("<div/>").addClass("auco_contact")[0];
+            ElementXBL.prototype.addBinding.call(node, "autocomplete.xml#contact-completion");
+            node.setContact(row.doc);
+            nodes.push(node);
+
+            seen[row.id] = true;
+          }
+        });
+        console.log("Want to tell dude about:", aAutocomplete);
+        aAutocomplete.haveSomeResults(aText, nodes);
+      }
+    });
+  }
+};
---
+++ client/cloda.js
@@ -0,0 +1,13 @@
+/**
+ * Cloda, it's a play on gloda and cloud.  Get it?!
+ */
+
+var Gloda = {
+  dbContacts: $.couch.db("contacts"),
+  dbMessages: $.couch.db("messages"),
+
+  _init: function () {
+
+  }
+};
+Gloda._init();
