16907fbce590b760ad44ca1517512076130bd821
Mark Hammond <mhammond@skippinet.com.au>
2009-03-12 22:31:14 +1100

    Use error-resistant protocol loading.
    --HG--
    branch : twisty

--- server/python/junius/proto/__init__.py
+++ server/python/junius/proto/__init__.py
@@ -1,7 +1,29 @@
-from .imap import IMAPAccount
-#from .twitter import TwitterAccount
+# this needs to become a 'plugin' mechanism...
 
-protocols = {
-    'imap': IMAPAccount,
-#    'twitter': TwitterAccount,
-}
+_protocol_infos = [
+    ('imap', 'junius.proto.imap', 'IMAPAccount'),
+    ('skype', 'junius.proto.skype', 'SkypeAccount'),
+    ('twitter', 'junius.proto.twitter', 'TwitterAccount'),
+]
+
+# it must be time I looked up dict-comps ;)
+protocols = {}
+def _load():
+    import sys, logging
+    logger = logging.getLogger('raindrop.proto')
+    for name, mod, factname in _protocol_infos:
+        try:
+            __import__(mod)
+            mod = sys.modules[mod]
+            fact = getattr(mod, factname)
+        except ImportError, why:
+            logger.info("Failed to import '%s' factory: %s", name, why)
+        except:
+            logger.exception("Error creating '%s' factory", name)
+        else:
+            protocols[name] = fact
+
+_load()
+del _load
+
+__all__ = protocols
