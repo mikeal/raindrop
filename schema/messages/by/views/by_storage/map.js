90fdc8bc55401091fca02e9e1317eba278ff542c
Mark Hammond <mhammond@skippinet.com.au>
2009-03-12 22:29:06 +1100

    only process 'rawMessage's
    --HG--
    branch : twisty

--- schema/messages/by/views/by_storage/map.js
+++ schema/messages/by/views/by_storage/map.js
@@ -1,5 +1,5 @@
 function(doc) {
-  if ((doc.type == 'message' || doc.type== 'rawMessage') && doc.subtype == "rfc822")
+  if (doc.type== 'rawMessage' && doc.subtype == "rfc822")
     if (doc.storage_path && doc.storage_id)
       emit([doc.account_id, doc.storage_path, doc.storage_id], null);
 }
