1bf956c3c3871cce2a237d6ec427d9e3761db6c8
Mark Hammond <mhammond@skippinet.com.au>
2009-03-25 19:14:16 +1100

    get the skype protocol using a view to optimize no of requests
    --HG--
    branch : twisty

--- server/python/junius/proto/skype.py
+++ server/python/junius/proto/skype.py
@@ -2,12 +2,7 @@
 '''
 Fetch skype contacts and chats.
 '''
-
-import base64, datetime
-import pprint
-import time
 import logging
-from urllib2 import urlopen
 
 import twisted.python.log
 from twisted.internet import defer, threads
@@ -17,8 +12,6 @@ from ..proc import base
 import Skype4Py
 
 logger = logging.getLogger(__name__)
-
-max_run = 4 # for the deferred semaphore...
 
 # These are the raw properties we fetch from skype.
 CHAT_PROPS = [
@@ -78,6 +71,9 @@ class TwistySkype(object):
         self.doc_model = doc_model
         self.skype = Skype4Py.Skype()
 
+    def finished(self, result):
+        return self.conductor.on_synch_finished(self.account, result)
+
     def get_docid_for_chat(self, chat):
         return "skypechat-" + chat.Name.encode('utf8') # hrmph!
 
@@ -98,9 +94,10 @@ class TwistySkype(object):
 
     def _cb_got_chats(self, chats):
         logger.debug("skype has %d chat(s) total", len(chats))
-        # chats is a tuple...
+        # 'chats' is a tuple
         self.remaining_chats = list(chats)
         return self.process_next_chat()
+
 
     def process_next_chat(self):
         if not self.remaining_chats:
@@ -109,37 +106,78 @@ class TwistySkype(object):
             return
 
         chat = self.remaining_chats.pop()
-        d = defer.Deferred()
-        # process the chat object itself if its not seen before.
-        d.addCallback(self._cb_maybe_process_chat, chat)
-        # process all messages in all chats - there may be new ones even if
-        # we previously have seen the chat itself.
-        d.addCallback(self._cb_process_chat_messages, chat)
-        d.addCallback(self._cb_processed_chat)
-        d.callback(None)
-        return d
+        # fetch all the messages (future optimization; all we need are the
+        # IDs, but we suffer from creating an instance wrapper for each item.
+        # Sadly the skype lib doesn't offer a clean way of doing this.)
+        return threads.deferToThread(chat._GetMessages
+                    ).addCallback(self._cb_got_messages, chat,
+                    )
 
-    def _cb_processed_chat(self, result):
-        # the 'pattern' we copied from the imap sample code calls for us
-        # to recurse - but that can recurse too far...
-        return self.conductor.reactor.callLater(0, self.process_next_chat)
+    def _cb_got_messages(self, messages, chat):
+        logger.debug("chat '%s' has %d message(s) total; looking for new ones",
+                     chat.Name, len(messages))
 
-    def _cb_maybe_process_chat(self, result, chat):
-        logger.debug("seeing if skype chat %r exists", chat.Name)
-        return self.doc_model.open_document(self.get_docid_for_chat(chat),
-                        ).addCallback(self._cb_process_chat, chat)
+        # Finally got all the messages for this chat.  Execute a view to
+        # determine which we have seen (note that we obviously could just
+        # fetch the *entire* view once - but we do it this way on purpose
+        # to ensure we remain scalable...)
+        return self.doc_model.db.openView('raindrop!proto!skype', 'seen',
+                                          startkey=[chat.Name],
+                                          endkey=[chat.Name, {}]
+                    ).addCallback(self._cb_got_seen, chat, messages
+                    )
 
-    def _cb_process_chat(self, existing_doc, chat):
-        if existing_doc is None:
-            logger.info("Creating new skype chat %r", chat.Name)
+    def _cb_got_seen(self, result, chat, messages):
+        self.msgs_by_id = dict((m._Id, m) for m in messages)
+        self.current_chat = chat
+        chatname = chat.Name
+        # The view gives us a list of [chat_name, msg_id], where msg_id is
+        # None if we've seen the chat itself.  Create a set of messages we
+        # *haven't* seen - including the [chat_name, None] entry if applic.
+        all_keys = [(chatname, None)]
+        all_keys.extend((chatname, mid) for mid in self.msgs_by_id.keys())
+        seen_chats = set([tuple(row['key']) for row in result])
+        self.remaining_items = set(all_keys)-set(seen_chats)
+        # we could just process the empty list as normal, but the logging of
+        # an info when we do have items is worthwhile...
+        if not self.remaining_items:
+            logger.debug("Chat %r has no new items to process", chatname)
+            self.msgs_by_id = None
+            return self.process_next_chat()
+        # we have something to do...
+        logger.info("Chat %r has %d items to process", chatname,
+                    len(self.remaining_items))
+        logger.debug("we've already seen %d items from this chat",
+                     len(seen_chats))
+        return self.process_next_item()
+
+    def process_next_item(self):
+        if not self.remaining_items:
+            self.msgs_by_id = None
+            logger.debug("finished processing messages for this chat")
+            return self.conductor.reactor.callLater(0, self.process_next_chat)
+
+        chatname, msgid = self.remaining_items.pop()
+        chat = self.current_chat
+        if msgid is None:
+            # we haven't seen the chat itself - do that.
+            logger.debug("Creating new skype chat %r", chat.Name)
             # make a 'deferred list' to fetch each property one at a time.
             ds = [threads.deferToThread(chat._Property, p)
                   for p, _ in CHAT_PROPS]
-            return defer.DeferredList(ds
+            ret = defer.DeferredList(ds
                         ).addCallback(self._cb_got_chat_props, chat)
         else:
-            logger.debug("Skipping skype chat %r - already exists", chat.Name)
-            # we are done.
+            msg = self.msgs_by_id[msgid]
+            # A new msg in this chat.
+            logger.debug("New skype message %d", msg._Id)
+            # make a 'deferred list' to fetch each property one at a time.
+            ds = [threads.deferToThread(msg._Property, p) for p, _ in MSG_PROPS]
+            ret = defer.DeferredList(ds
+                        ).addCallback(self._cb_got_msg_props, chat, msg)
+
+        ret.addCallback(self._cb_doc_saved)
+        return ret
 
     def _cb_got_chat_props(self, results, chat):
         logger.debug("got chat %r properties: %s", chat.Name, results)
@@ -157,64 +195,22 @@ class TwistySkype(object):
                                                   self.account
                     )
 
-    def finished(self, result):
-        return self.conductor.on_synch_finished(self.account, result)
-
-    # Processing the messages for each chat...
-    def _cb_process_chat_messages(self, result, chat):
-        # Get each of the messages in the chat
-        return threads.deferToThread(chat._GetMessages
-                    ).addCallback(self._cb_got_messages, chat,
-                    )
-
-    def _cb_got_messages(self, messages, chat):
-        logger.debug("chat '%s' has %d message(s) total; looking for new ones",
-                     chat.Name, len(messages))
-        self.remaining_messages = list(messages) # message is a tuple.
-        self.current_chat = chat
-        return self.process_next_message()
-
-    def process_next_message(self):
-        if not self.remaining_messages:
-            logger.debug("finished processing messages for this chat")
-            return
-
-        msg = self.remaining_messages.pop()
-        chat = self.current_chat
-        logger.debug("seeing if message %r exists (in chat %r)",
-                     msg._Id, chat.Name)
-        return self.doc_model.open_document(self.get_docid_for_msg(msg)
-                        ).addCallback(self._cb_maybe_process_message, chat, msg
-                        ).addCallback(self._cb_processed_message
-                        )
-
-    def _cb_processed_message(self, result):
-        return self.conductor.reactor.callLater(0, self.process_next_message)
-
-    def _cb_maybe_process_message(self, existing_doc, chat, msg):
-        if existing_doc is None:
-            logger.debug("New skype message %d", msg._Id)
-            # make a 'deferred list' to fetch each property one at a time.
-            ds = [threads.deferToThread(msg._Property, p) for p, _ in MSG_PROPS]
-            return defer.DeferredList(ds
-                        ).addCallback(self._cb_got_msg_props, chat, msg)
-        else:
-            logger.debug("already have raw doc for msg %r; skipping", msg._Id)
-
     def _cb_got_msg_props(self, results, chat, msg):
         doc = {}
         for (name, typ), (ok, val) in zip(MSG_PROPS, results):
             if ok:
                 doc['skype_'+name.lower()] = simple_convert(val, typ)
+        doc['skype_id'] = msg._Id
         # we include the skype username with the ID as they are unique per user.
         docid = self.get_docid_for_msg(msg)
         return self.doc_model.create_raw_document(docid, doc, 'proto/skype-msg',
                                                   self.account
-                    ).addCallback(self._cb_msg_saved
                     )
 
-    def _cb_msg_saved(self, result):
-        pass
+    def _cb_doc_saved(self, result):
+        # the 'pattern' we copied from the imap sample code calls for us
+        # to recurse - but that can recurse too far...
+        return self.conductor.reactor.callLater(0, self.process_next_item)
 
 # A 'converter' - takes a proto/skype-msg as input and creates a
 # 'message' as output (some other intermediate step might end up more
