4eacf56f12ccef8e10e0ce885caae571f3031bfc
Mark Hammond <mhammond@skippinet.com.au>
2009-04-03 15:54:11 +1100

    comment about lack of unicode in tag hacks
    --HG--
    branch : twisty

--- server/python/junius/ext/message/message.py
+++ server/python/junius/ext/message/message.py
@@ -7,6 +7,12 @@ logger = logging.getLogger(__name__)
 
 from ...proc import base
 
+# *sob* this re still fails to get unicode.
+# | >>> re.compile(r"[^\w]+", re.UNICODE).split(u"\xa9opyright me")
+# | [u'', u'opyright', u'me']
+re_tags = re.compile(r"[^\w]+", re.UNICODE)
+
+
 class MessageAnnotator(base.ConverterBase):
     def convert(self, doc):
         # for now, if a 'proto' couldn't detect tags by itself, all words in
@@ -14,7 +20,7 @@ class MessageAnnotator(base.ConverterBase):
         try:
             tags = doc['tags']
         except KeyError:
-            bits = re.split(r"[^A-Za-z]+", doc['body'])
+            bits = re_tags.split(doc['body'])
             tags = list(set(b.lower() for b in bits if len(b)>3))
         conversation_id = doc.get('conversation_id')
         if conversation_id is None:
