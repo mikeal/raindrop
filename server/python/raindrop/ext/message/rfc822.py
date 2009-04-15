ce21496218206dfbe35bb9c40b9b6d860829a3d7
Bryan Clark <clarkbw@gnome.org>
2009-04-14 17:11:20 -0700

    use internationalized header decoding from email.header.decode_header http://docs.python.org/library/email.header.html#email.header.decode_header
    The function naming might not be the best, I just tried to follow the pattern

--- server/python/raindrop/ext/message/rfc822.py
+++ server/python/raindrop/ext/message/rfc822.py
@@ -5,6 +5,7 @@ from __future__ import absolute_import # stop 'email' import finding our ext
 import logging
 from email import message_from_string
 from email.utils import mktime_tz, parsedate_tz
+from email.header import decode_header
 from twisted.internet import defer
 
 from ...proc import base
@@ -30,6 +31,19 @@ def _safe_convert_bytes(val, charset):
             ret = val.decode('utf-8', 'ignore')
     return ret
 
+def _safe_convert_header_bytes(val, charset):
+    # decode_header returns a list of tuples with guessed encoding values
+    # we try to decode the list of strings using suggested encoding or utf-8
+    # then merge the decoded pieces into one utf-8 string we can return
+    try:
+        parts = decode_header(val)
+        encoded_parts = [part.decode(encoding or "utf-8") for part, encoding in parts]
+        ret = u"".join(encoded_parts)
+    except (LookupError, UnicodeError), exc:
+        logger.error("Failed to decode mail headers: %s", exc)
+        # couldn't convert so lets fall back to the default converter
+        ret = _safe_convert_bytes(val, charset)
+    return ret
 
 # an 'rfc822' message stores the unpacked version of the rfc822 stream, a-la
 # the interface provided by the 'email' package.  IOW, we never bother storing
@@ -49,7 +63,7 @@ def doc_from_bytes(b):
     # Who wins?  We try the content encoding first then fallback to utf.
     charset = msg.get_charset() or msg.get_content_charset()
     for hn, hv in msg.items():
-        headers[hn.lower()] = _safe_convert_bytes(hv, charset)
+        headers[hn.lower()] = _safe_convert_header_bytes(hv, charset)
 
     # XXX - technically msg objects are recursive; handling that requires
     # more thought.  For now, assume they are flat.
