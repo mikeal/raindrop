73e51ab049822173e81d5a47036ca921a7084bb9
Bryan Clark <clarkbw@gnome.org>
2009-04-16 13:26:50 -0700

    tune the message limit down to 30 from 100 for a more reasonable set make the message.js name, address, subject hacks more robust for tweets improve the message.css so empty subjects don't take space

--- client/rdw/Message.js
+++ client/rdw/Message.js
@@ -23,15 +23,27 @@ dojo.declare("rdw.Message", [rdw._Base], {
     //TODO: some of these need more info from backend.
     this.userPicUrl = this.blankImgUrl;
     // XXX: theese are a couple hacks to get the UI looking more like we want
-    this.fromName = rd.escapeHtml(this.doc.from[1].split("<")[0]);
+    this.fromName = this.doc.from[1];
+    try {
+      var pieces = this.doc.from[1].split("<");
+      if(pieces && pieces[0]) {
+        this.fromName = pieces[0];
+      }
+    } catch(ignore) { }
     
     this.fromId = this.doc.from[1];
-    var matches = this.doc.from[1].match(/<(.+)>/);
-    if(matches && matches[1]) {
-      this.fromId = matches[1].toLowerCase();
-    }
+    try {
+      var matches = this.doc.from[1].match(/<(.+)>/);
+      if(matches && matches[1]) {
+        this.fromId = matches[1].toLowerCase();
+      }
+    } catch(ignore) { }
 
-    this.subject = rd.escapeHtml(this.doc.subject.replace(/^Re:/,''));
+    this.subject = null;
+    try {
+      this.subject = rd.escapeHtml(this.doc.subject.replace(/^Re:/,''));
+    } catch(ignore_empty_subjects) { }
+
     this.message = rd.escapeHtml(this.doc.body_preview);
     this.time = 0;
     this.timeDisplay = rd.escapeHtml("some time ago");
