70a0bbb0e226e2115ae10ce8ed088d09ca7eb1e4
Bryan Clark <clarkbw@gnome.org>
2009-04-15 18:22:33 -0700

    update the search widget remove the whole border for an input border added the recent search place holders pulled in the firefox (only) search icon

--- client/rdw/Search.js
+++ client/rdw/Search.js
@@ -12,5 +12,24 @@ dojo.declare("rdw.Search", [rdw._Base], {
   postCreate: function() {
     //summary: dijit lifecycle method
     this.inherited("postCreate", arguments);
+  },
+  focusSearchInput: function(evt) {
+    //summary: keep the search area alive when focused but the mouse left
+    dojo.addClass(this.search, 'searchInputFocused');
+  },
+  blurSearchInput: function(evt) {
+    //summary: reset the search area opacity when focus leaves
+    dojo.removeClass(this.search, 'searchInputFocused');
+  },
+  onClick: function(evt) {
+    //summary: handles click delegation for list of recent search links
+    var target = evt.target;
+    if (target.href) {
+      target = target.href.split("#")[1];
+      if (target) {
+        dojo.publish("rd-nav-" + target);
+        dojo.stopEvent(evt);
+      }
+    }
   }
 });
