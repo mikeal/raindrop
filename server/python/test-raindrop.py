dfe67487a555a7330731daf15efdc844add0cd7f
Mark Hammond <mhammond@skippinet.com.au>
2009-04-20 11:22:11 +1000

    simple test driver

---
+++ server/python/test-raindrop.py
@@ -0,0 +1,11 @@
+# Twisted's 'trial' package has its own idea of logging and cmdline args etc -
+# we still need to work out how to best integrate our world and theirs - for
+# now we just do the simplest thing possible...
+
+import sys
+# If no args specified run all tests...
+if len(sys.argv)==1:
+    sys.argv.append('raindrop.tests')
+
+from twisted.scripts.trial import run
+run()
