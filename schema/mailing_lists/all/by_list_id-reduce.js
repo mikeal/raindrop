7081932ddb7c5748a7747b3e9a6ce9bd249ba629
Myk Melez <myk@mozilla.org>
2009-04-24 00:36:53 -0700

    remove obsolete code from 'mailing lists: by list ID' query
    --HG--
    extra : rebase_source : 2d2bae8ce387fd3a5bb71f2ecc54169e80755674

--- schema/mailing_lists/all/by_list_id-reduce.js
+++ schema/mailing_lists/all/by_list_id-reduce.js
@@ -1,13 +1,3 @@
 function(keys, values, rereduce) {
   return values[0];
 }
-//function(keys, values, rereduce) {
-//  var output = {};
-//  output.count = values.length;
-//  for (var idx in values) {
-//    for (var elm in values[idx]) {
-//      output[elm] = values[idx][elm];
-//    }
-//  }
-//  return output;
-//}
