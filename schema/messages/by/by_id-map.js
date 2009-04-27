3cfc21ac10cdfd867e71631360053f4e0a80d324
David Ascher <david@mozillamessaging.com>
2009-04-27 09:34:45 -0700

    forgotten map

---
+++ schema/messages/by/by_id-map.js
@@ -0,0 +1,10 @@
+/* used to do the grouped queries of all documents in a message chain */
+
+
+function(doc) {
+  var id = doc['_id'];
+  if (id.indexOf('msg') == 0) { // XXX hack?
+    [msg, base, msgtype] = id.split('!', 2);
+    emit(msg+'!'+base, doc['type']);
+  }
+}
