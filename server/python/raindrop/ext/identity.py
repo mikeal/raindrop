48b39b7aa3affecc84bd4a9417d771632ca2aad9
Mark Hammond <mhammond@skippinet.com.au>
2009-05-01 11:36:25 +1000

    New identity converters to we always end up with a doc of type='identity'
    --HG--
    extra : rebase_source : b23898575a7a84213553b72956f92245007b807f

--- server/python/raindrop/ext/identity.py
+++ server/python/raindrop/ext/identity.py
@@ -1,5 +1,30 @@
 from ..proc import base
 
+# Our identities 'phone' and 'url' are 'simple' in that there is currently
+# no further information we can extract.
+def do_simple_id_convert(doc):
+    return {
+        'name' : doc['identity_id'][1],
+        'identity_id': ['identity_id'],
+    }
+
+    
+class PhoneIdentityConverter(base.SimpleConverterBase):
+    target_type = 'id', 'identity'
+    sources = [('id', 'phone')]
+
+    def simple_convert(self, doc):
+        return do_simple_id_convert(doc)
+
+
+class URLIdentityConverter(base.SimpleConverterBase):
+    target_type = 'id', 'identity'
+    sources = [('id', 'url')]
+
+    def simple_convert(self, doc):
+        return do_simple_id_convert(doc)
+
+
 class MessageAnnotator(base.SimpleConverterBase):
     target_type = 'msg', 'anno/tags'
     # I only consume one type of object.
