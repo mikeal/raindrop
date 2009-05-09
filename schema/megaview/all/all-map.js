eab791dce147ec29aa03423b7fdd43e97ddd3c81
James Burke <jrburke@gmail.com>
2009-05-08 21:13:13 -0700

    For megaview: put doc type as first part of emitted key, instead of hardcoding ignoring of body property, use a string length of over 140 characters. Should avoid the large bodies in the view. Also ignore documents that have doc._ignore_megaview = true.

--- schema/megaview/all/all-map.js
+++ schema/megaview/all/all-map.js
@@ -1,14 +1,21 @@
 function(doc) {
   if(doc.type
+    && !doc._ignore_megaview
     && doc.type.indexOf("account") != 0
     && doc.type.indexOf("ghost") != 0
     && doc.type.indexOf("raw") == -1
     && doc.type.indexOf("workqueue") != 0
     && doc.type.indexOf("ui") != 0) {
     for (var prop in doc) {
-        if(prop.indexOf("_") != 0 && prop.indexOf("body") != 0 && prop.indexOf("raindrop") != 0) {
-          emit([prop, doc[prop], doc.type, (doc.timestamp || 0)], doc._id)
-      }
+        //Skip text fields that are big (better served by full
+        //text search), private props and raindrop-housekeeping
+        //props.
+        if (((typeof doc[prop] == "string") && doc[prop].length > 140)
+             || prop.charAt(0) == "_"
+             || prop.indexOf("raindrop") == 0) {
+          continue;
+        }
+        emit([doc.type, prop, doc[prop], (doc.timestamp || 0)], null)
     }
   }
 }
