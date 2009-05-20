c4eeb9971cd1d1b5f92fbff26a36df0bd6eb7d8e
Mark Hammond <mhammond@skippinet.com.au>
2009-05-20 14:52:54 +1000

    update mega-view to new world order

--- schema/megaview/all/all-map.js
+++ schema/megaview/all/all-map.js
@@ -1,21 +1,20 @@
 function(doc) {
-  if(doc.type
-    && !doc._ignore_megaview
-    && doc.type.indexOf("account") != 0
-    && doc.type.indexOf("ghost") != 0
-    && doc.type.indexOf("raw") == -1
-    && doc.type.indexOf("workqueue") != 0
-    && doc.type.indexOf("ui") != 0) {
+  if (doc.rd_schema_id
+    && !doc.rd_ignore_megaview
+    && doc.rd_schema_id.indexOf("raw") == -1
+    && doc.rd_schema_id.indexOf("workqueue") != 0
+    && doc.rd_schema_id.indexOf("ui") != 0) {
     for (var prop in doc) {
         //Skip text fields that are big (better served by full
         //text search), private props and raindrop-housekeeping
         //props.
         if (((typeof doc[prop] == "string") && doc[prop].length > 140)
              || prop.charAt(0) == "_"
+             || prop.indexOf("rd_") == 0
              || prop.indexOf("raindrop") == 0) {
           continue;
         }
-        emit([doc.type, prop, doc[prop], (doc.timestamp || 0)], null)
+        emit([doc.rd_schema_id, prop, doc[prop], (doc.timestamp || 0)], doc._rev)
     }
   }
 }
