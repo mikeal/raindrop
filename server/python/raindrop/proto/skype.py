9f54b1a191563e88dd45a8dd5fd36cbc8f675865
Mark Hammond <mhammond@skippinet.com.au>
2009-05-20 14:55:05 +1000

    fix whitespace and comments

--- server/python/raindrop/proto/skype.py
+++ server/python/raindrop/proto/skype.py
@@ -270,22 +270,22 @@ class TwistySkype(object):
                             'ext_id': self.rd_extension_id})
         return self.doc_model.create_schema_items(schemas)
 
-# A 'converter' - takes a rd/msg/proto/skype-msg as input and creates various
+# A 'converter' - takes a rd/msg/skypemsg/raw as input and creates various
 # schema outputs for that message - specifically 'body' and 'conversation'
 @base.raindrop_extension('rd/msg/skypemsg/raw')
 def skype_converter(doc):
-        subject = doc['skype_chat_friendlyname']
-        # Currently 'body' also defines 'envelope' type items
-        bdoc = {'from': ['skype', doc['skype_from_handle']],
-                'subject': subject,
-                'body': doc['skype_body'],
-                'body_preview': doc['skype_body'][:140],
-                'timestamp': doc['skype_timestamp'], # skype's works ok here?
-                }
-        emit_schema('rd/msg/body', bdoc)
-        # and a conversation schema
-        cdoc = {'conversation_id': doc['skype_chatname']}
-        emit_schema('rd/msg/conversation', cdoc)
+    subject = doc['skype_chat_friendlyname']
+    # Currently 'body' also defines 'envelope' type items
+    bdoc = {'from': ['skype', doc['skype_from_handle']],
+            'subject': subject,
+            'body': doc['skype_body'],
+            'body_preview': doc['skype_body'][:140],
+            'timestamp': doc['skype_timestamp'], # skype's works ok here?
+            }
+    emit_schema('rd/msg/body', bdoc)
+    # and a conversation schema
+    cdoc = {'conversation_id': doc['skype_chatname']}
+    emit_schema('rd/msg/conversation', cdoc)
 
     
 _skype_id_converter_skype = None
