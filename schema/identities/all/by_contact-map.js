50257e31feee1fe41c7e8139e8f1ba7a654e6848
Mark Hammond <mhammond@skippinet.com.au>
2009-05-21 20:23:01 +1000

    identities-by-contact view working under new world order

--- schema/identities/all/by_contact-map.js
+++ schema/identities/all/by_contact-map.js
@@ -1,6 +1,6 @@
 function(doc) {
-  if (doc.type=='contacts')
+  if (doc.rd_schema_id == 'rd/identity/contacts')
     for each (var [contact_id, rel] in doc.contacts) {
-      emit([contact_id, rel], doc.identity_id);
+      emit([contact_id, rel], doc.rd_key);
     }
 }
