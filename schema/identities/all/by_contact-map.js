18091c85adbfa39d33d90296b4b433757a03a832
Mark Hammond <mhammond@skippinet.com.au>
2009-05-22 14:39:19 +1000

    update comments

--- schema/identities/all/by_contact-map.js
+++ schema/identities/all/by_contact-map.js
@@ -1,6 +1,10 @@
 function(doc) {
   if (doc.rd_schema_id == 'rd/identity/contacts')
     for each (var [contact_id, rel] in doc.contacts) {
+      // Although rd_key[0] must always be 'identity', we still return
+      // the entire key to try and keep the abstraction at the 'rd_key'
+      // level (ie, if we remove 'identity' the front-end etc will just
+      // need to re-add it should it want to lookup a schema for the ID...)
       emit([contact_id, rel], doc.rd_key);
     }
 }
