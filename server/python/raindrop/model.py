8a8d03a9b006f2c1e5f7b15d36cb08931cc1b101
Mark Hammond <mhammond@skippinet.com.au>
2009-07-17 14:56:33 +1000

    make to and cc etc 'megaview_expandable'

--- server/python/raindrop/model.py
+++ server/python/raindrop/model.py
@@ -53,6 +53,7 @@ else:
 megaview_schemas_expandable_values = {
     'rd.tags' : ['tags'],
     'rd.identity.contacts' : ['contacts'],
+    'rd.msg.body' : ['to', 'to_display', 'cc', 'cc_display']
 }
 # Ditto - info which should come from the schema-defn itself - a list of
 # schemas that don't need values emitted, just the keys etc.
