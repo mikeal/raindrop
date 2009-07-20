96e8355e6c0ecfca8e16ad2e6a8ee5e081350f92
James Burke <jrburke@gmail.com>
2009-07-20 14:41:50 -0700

    Changes for DnD handling.

--- client/lib/rdw/Organizer.js
+++ client/lib/rdw/Organizer.js
@@ -3,10 +3,13 @@ dojo.provide("rdw.Organizer");
 dojo.require("rd.conversation");
 dojo.require("rdw._Base");
 dojo.require("rdw.MailingList");
+dojo.require("dojo.dnd.Source");
 
 dojo.declare("rdw.Organizer", [rdw._Base], {
   // Mailing lists to which any messages in the datastore belong.
   // Populated by a view after the widget gets created.
+  widgetsInTemplate: true,
+
   _mailingLists: null,
   mailingLists: function(newVal) {
     if (!newVal)
@@ -15,26 +18,34 @@ dojo.declare("rdw.Organizer", [rdw._Base], {
     this._mailingLists = newVal;
 
     // Remove existing mailing list widgets from the presentation.
-    rd.query(".MailingList", this.list).forEach(function(mailingList) {
+    rd.query(".MailingList", this.listNode).forEach(function(mailingList) {
       rd.destroy(mailingList);
     });
 
     //Use a document fragment for best performance
     //and load up each story widget in there.
     var frag = dojo.doc.createDocumentFragment();
+    var dndNodes = [];
     dojo.forEach(this._mailingLists, dojo.hitch(this, function(doc) {
       //Store widgets in this._supportingWidgets so they get destroyed properly
       //by dijit infrastructure.
       if (!this._supportingWidgets) {
         this._supportingWidgets = [];
       }
-      this._supportingWidgets.push(new rdw.MailingList({
-        doc: doc
-      }, dojo.create("div", null, frag)));
+      var widget = new rdw.MailingList({
+          doc: doc,
+          "class": "dojoDndItem"
+        }, dojo.create("div", null, frag));
+      
+      this._supportingWidgets.push(widget);
+      dndNodes.push(widget.domNode);
     }));
 
     //Inject nodes all at once for best performance.
-    this.list.appendChild(frag);
+    this.listNode.appendChild(frag);
+
+    //Register newly created nodes for DND
+    this.dndSource.insertNodes(false, dndNodes);
 
     // Return anything to suppress JavaScript strict warnings about the function
     // not always returning a value.
@@ -52,6 +63,8 @@ dojo.declare("rdw.Organizer", [rdw._Base], {
     //summary: dijit lifecycle method
     this.inherited("postCreate", arguments);
 
+    this.dndSource = new dojo.dnd.Source(this.listNode);
+
     // Populate the widget with a list of mailing lists to which any messages
     // in the datastore belong.
     couch.db("raindrop").view("raindrop!content!all/_view/megaview", {
@@ -66,5 +79,11 @@ dojo.declare("rdw.Organizer", [rdw._Base], {
         this.mailingLists(lists);
       })
     });
+  },
+  
+  destroy: function() {
+    //summary: dijit lifecycle method
+    this.dndSource.destroy();
+    this.inherited("destroy", arguments);
   }
 });
