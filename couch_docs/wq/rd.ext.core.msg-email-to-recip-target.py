87ffb9f8b1cdfa623f59c031c86a42b129129673
Mark Hammond <mhammond@skippinet.com.au>
2009-07-22 11:11:28 +1000

    add 'from' as a recip target, and fix tuple/list confusion

--- couch_docs/wq/rd.ext.core.msg-email-to-recip-target.py
+++ couch_docs/wq/rd.ext.core.msg-email-to-recip-target.py
@@ -9,13 +9,15 @@ def handler(src_doc):
     my_identities = get_my_identities()
     to = src_doc.get('to', [])
     val = None
+    if src_doc.get('from') in my_identities:
+        val = 'from'
     # only us on the 'to' line?  That is 'direct'...
-    if len(to)==1 and tuple(to[0]) in my_identities:
+    elif len(to)==1 and to[0] in my_identities:
         val = 'direct'
     else:
         look = to + src_doc.get('cc', [])
         for maybe in look:
-            if tuple(maybe) in my_identities:
+            if maybe in my_identities:
                 val = 'group'
                 break
         else:
