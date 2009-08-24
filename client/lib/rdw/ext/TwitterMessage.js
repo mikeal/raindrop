6415dd555b114471beec8d22e7819ba96a67fb5c
Bryan Clark <clarkbw@gnome.org>
2009-08-24 14:21:32 -0700

    remove twitter debug

--- client/lib/rdw/ext/TwitterMessage.js
+++ client/lib/rdw/ext/TwitterMessage.js
@@ -10,7 +10,6 @@ dojo.require("rd.hyperlink");
 rd.applyExtension("rdw.ext.TwitterMessage", "rdw.Message", {
   after: {
     postMixInProperties: function() {
-      console.log(this.messageBag);
       if(this.messageBag['rd.msg.body'].from[0] == "twitter") {
         this.message = rd.hyperlink.addTwitterUsers(this.message);
         this.message = rd.hyperlink.addTwitterTags(this.message);
