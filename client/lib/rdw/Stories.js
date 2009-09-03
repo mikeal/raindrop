4d37fec5f778a938c214532d3743499cc5d873ed
James Burke <jrburke@mozillamessaging.com>
2009-09-02 17:36:39 -0700

    Adding in support in the home view for only showing unseen/unarchived/undeleted messages. Also fixed a click selection issue for the active stuff.

--- client/lib/rdw/Stories.js
+++ client/lib/rdw/Stories.js
@@ -94,12 +94,20 @@ dojo.declare("rdw.Stories", [rdw._Base], {
     }
   },
 
+  //elements to ignore for click selection.
+  clickIgnore: {
+    "input": 1,
+    "textarea": 1,
+    "select": 1,
+    "option": 1
+  },
+
   onClick: function(/*Event*/evt) {
     //summary: handles click events, tries to select the first selectable item
     //relative to the click.
     var target = evt.target;
     do {
-      if (target == this.domNode) {
+      if (target == this.domNode || this.clickIgnore[target.nodeName.toLowerCase()]) {
         break;
       }
       if (target.tabIndex > -1) {
