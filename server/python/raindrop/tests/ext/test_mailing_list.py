005772e52b1833baafeea39f13f0529ef88f4a29
Myk Melez <myk@mozilla.org>
2009-09-04 15:36:47 -0700

    list the test cases i want the mailing list unit test script to eventually run

--- server/python/raindrop/tests/ext/test_mailing_list.py
+++ server/python/raindrop/tests/ext/test_mailing_list.py
@@ -2,6 +2,46 @@ from twisted.internet import defer
 
 from raindrop.tests import TestCaseWithCorpus
 
+# Cases to test:
+#
+# * a message from a list: the rd.msg.email.mailing-list and rd.mailing-list
+#   docs should be created with the appropriate properties;
+#
+# * a message that updates a list: the list properties should be updated;
+#
+# The rest of these testcases need to be run separately for Google Groups
+# and Mailman lists, since the unsubscription code for those two kinds of lists
+# isn't shared between them.  At some point it might be possible to factor out
+# some of their code, however, at which point we may be able to merge some of
+# the tests.
+#
+# * a message from a list followed by a newer unsubscribe confirmation:
+#   the list state should become "unsubscribed";
+#
+# * a message from a list followed by an older unsubscribe confirmation:
+#   the list state should remain "subscribed";
+#
+# * an unsubscribe confirmation followed by a newer message from the list:
+#   the list state should become "subscribed";
+#
+# * an unsubscribe confirmation followed by an older message from the list:
+#   the list state should remain "unsubscribed";
+#
+# * an unsubscribe confirmation for a non-existing list: the list doc should
+#   be created, and its state should be "unsubscribed" (because we expect to
+#   later process some older messages from the list and want to make sure they
+#   show up as being from an unsubscribed list);
+#
+# * an unsubscribe confirmation for a non-existing list followed by an older
+#   message from the list: the list state should remain "unsubscribed";
+#
+# * an unsubscribe confirmation for a non-existing list followed by a newer
+#   message from the list: the list state should become "subscribed";
+#
+# * an unsubscribe confirm request for a list in the "unsubscribe-pending"
+#   state: an rd.msg.outgoing.simple doc should be created with the appropriate
+#   properties; the list state should become "unsubscribe-confirmed";
+
 class TestSimpleCorpus(TestCaseWithCorpus):
     @defer.inlineCallbacks
     def test_simple_mailing_list(self):
