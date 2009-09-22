2040d30cad5728e175b5c5e09c8cc75e2866f658
James Burke <jrburke@mozillamessaging.com>
2009-09-21 17:14:17 -0700

    small detail cleanup, and enable showing selected list in the Organizer. Still needs styling help from Andy, but the mechanics of it work.

--- client/lib/rdw/Organizer.js
+++ client/lib/rdw/Organizer.js
@@ -27,6 +27,14 @@ dojo.declare("rdw.Organizer", [rdw._Base], {
 
     this.dndSource = new dojo.dnd.Source(this.listNode);
 
+
+    //Listen for different links already in the template, for current
+    //selection changes.
+    dojo.query("li[type]").forEach(function(node) {
+      var type = node.getAttribute("type");
+      this.subscribe("rd-protocol-" + type, dojo.hitch(this, "onSelectionChange", type));
+    }, this);
+
     //Generate a list of links for the Organizer.
     //Use a name dispatch convention to allow extensions
     //to add other links.
@@ -79,7 +87,7 @@ dojo.declare("rdw.Organizer", [rdw._Base], {
     rd.tag.locations(dojo.hitch(this, function(locations) {
       var html = "";
       for (var i = 0, loc; loc = locations[i]; i++) {
-        html += dojo.string.substitute('<li class="locationTag dojoDndItem"><a href="#rd:locationTag:${id}">${name}</a></li>', {
+        html += dojo.string.substitute('<li type="locationTag:${id}" class="locationTag dojoDndItem"><a href="#rd:locationTag:${id}">${name}</a></li>', {
           id: loc.toString(),
           name: loc.join("/")
         });
@@ -87,13 +95,53 @@ dojo.declare("rdw.Organizer", [rdw._Base], {
 
       if (html) {
         this.addItems("listLocation", "Mail Folders", dojo._toDom(html));
+
+        //Listen to set current selection state.
+        this.subscribeSelection("locationTag");
       }
     }));
+  },
+
+  subscribeSelection: function(/*String*/protocolSubType) {
+    //summary: subscribes to the selection notification for the rd-protocol topic
+    //with the provided sub type.
+    this.subscribe("rd-protocol-" + protocolSubType, dojo.hitch(this, "onSelectionChange", protocolSubType));
+    
+    //Check current URL fragment. If contains the protocol sub type, then set selection.
+    var fragId = location.href.split("#")[1];
+    var prefix = "rd:" + protocolSubType + ":";
+    if (fragId && fragId.indexOf(prefix) == 0) {
+      var args = fragId.substring(prefix.length, fragId.length);
+      args = (args && args.split(":")) || [];
+      args.unshift(protocolSubType);
+      this.onSelectionChange.apply(this, args);
+    }
+  },
+
+  onSelectionChange: function(/*String*/type, /*String*/arg) {
+    //summary: as the selection changes, highlight the right thing in the organizer.
+    //Can be called with no args, which means try applying previous stored
+    //this.selectionType. Useful for extensions that add new things after an
+    //async API call.
+
+    if (type || this.selectionType) {
+      //First remove current selection.
+      dojo.query("li.selected", this.domNode).removeClass("selected");
+  
+      //Apply selection to the correct element.
+      if (type) {
+        this.selectionType = Array.prototype.join.call(arguments, ":");
+      }
+      if (this.selectionType) {
+        dojo.query('[type="' + this.selectionType + '"]').addClass("selected");
+      }
+    }
   },
 
   destroy: function() {
     //summary: dijit lifecycle method
     this.dndSource.destroy();
+    delete this.dndSource;
     this.inherited("destroy", arguments);
   }
 });
