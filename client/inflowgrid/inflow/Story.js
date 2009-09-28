0084942c64ad6dd064502a53b7ecd5357897d092
James Burke <jrburke@mozillamessaging.com>
2009-09-28 15:56:19 -0700

    more grd inflow scaffolding, for message templates.

--- client/inflowgrid/inflow/Story.js
+++ client/inflowgrid/inflow/Story.js
@@ -1,11 +1,17 @@
 dojo.provide("inflow.Story");
 
 dojo.require("rdw.Story");
+dojo.require("inflow.Message");
 
 dojo.declare("inflow.Story", [rdw.Story], {
   templateString: '<div class="rdwStory" dojoAttachEvent="onclick: onClick"> \
                     <div class="messages" dojoAttachPoint="containerNode"></div> \
                     <div class="toolAction" dojoAttachPoint="toolDisplayNode"> \
                     </div> \
-                  </div>'
+                  </div>',
+
+  //The name of the constructor function (module) that should be used
+  //to show individual messages.
+  messageCtorName: "inflow.Message"
+
 });
