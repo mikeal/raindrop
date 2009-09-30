9a947d92c02eae527f83ca197bd65f7f2d692e28
James Burke <jrburke@mozillamessaging.com>
2009-09-30 12:25:42 -0700

    Give separate inflowgrid template for QuickCompose, and reroute rdw.css paths to be inflowgrid/css paths.

--- client/inflowgrid/inflowgrid/QuickCompose.js
+++ client/inflowgrid/inflowgrid/QuickCompose.js
@@ -3,6 +3,8 @@ dojo.provide("inflowgrid.QuickCompose");
 dojo.require("rdw.QuickCompose");
 
 dojo.declare("inflowgrid.QuickCompose", [rdw.QuickCompose], {
+  templatePath: dojo.moduleUrl("inflowgrid.templates", "QuickCompose.html"),
+
   onCloseClick: function(evt) {
     inflowgrid.hideQuickCompose();
     dojo.stopEvent(evt);
