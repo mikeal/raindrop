bc03afddd10244e1aa16f3ed97d9ed181de62f0e
Mark Hammond <mhammond@skippinet.com.au>
2009-11-04 17:59:18 +1100

    new entry points and optimizations of existing ones

--- couch_docs/api/inflow.py
+++ couch_docs/api/inflow.py
@@ -30,6 +30,24 @@ class API:
     # A base class - helpers for the implementation classes...
     def get_args(self, req, *req_args, **opt_args):
         supplied = req['query']
+        body = req.get('body')
+        if body and body != 'undefined': # yes, couch 0.10 send the literal 'undefined'
+            # If there was a body specified, it can also contain request
+            # args - although no args can be specified in both places.
+            try:
+                body_args = json.loads(body)
+                if not isinstance(body_args, dict):
+                    raise ValueError, "body must be a json object"
+            except ValueError, why:
+                raise APIErrorResponse(400, "invalid json in request body: %s" % (why,))
+            s = set(body_args)
+            both = set.union(set(supplied))
+            if both:
+                msg = "the following arguments appear in both the query string and post body: %s" \
+                      % (",".join(both))
+                raise APIErrorResponse(400, msg)
+            supplied.update(body_args)
+
         ret = {}
         for arg in req_args:
             if arg not in supplied:
@@ -47,13 +65,17 @@ class API:
         if req['verb'] != 'GET':
             raise APIErrorResponse(405, "GET required")
 
+    def requires_get_or_post(self, req):
+        if req['verb'] not in ['GET', 'POST']:
+            raise APIErrorResponse(405, "GET or POST required")
+
 
 # The classes which define the API; all methods without a leading _ are public
 class ConversationAPI(API):
-    def _filter_fresh(self, db, docs):
+    def _filter_fresh(self, db, rdkeys):
         keys = []
         fresh = set()
-        for rd_key in [d['rd_key'] for d in docs]:
+        for rd_key in rdkeys:
             keys.append(["rd.core.content", "key-schema_id", [rd_key, "rd.msg.archived"]]);
             keys.append(["rd.core.content", "key-schema_id", [rd_key, "rd.msg.deleted"]]);
             fresh.add(hashable_key(rd_key))
@@ -68,32 +90,24 @@ class ConversationAPI(API):
                     pass
 
         # Now weed out the not-fresh from the result.
-        return [doc for doc in docs if hashable_key(doc['rd_key']) in fresh]
+        return [rdkey for rdkey in rdkeys if hashable_key(rd_key) in fresh]
 
-    def _fetch_contact_identities(self, db, idids):
+    def _filter_known_identities(self, db, idids):
+        # Given a list/set of identity IDs, return those 'known' (ie, associated
+        # with a contact.
         result = db.megaview(key=["rd.core.content", "schema_id", "rd.identity.contacts"],
-                              reduce=False, include_docs=True)
+                             reduce=False, include_docs=False)
         # Cycle through each document. The rd_key is the full identity ID, we just
         # need the second part of it. It has an array of contacts but each item
         # in the contacts array has other info about the contact, we just need
         # the first part, the contactId.
-        by_contact = {}
-        by_idty = {}
+        all_known = set()
         for row in result['rows']:
-            doc = row['doc']
-            idty = doc['rd_key'][1]
+            idty = row['value']['rd_key'][1]
             idkey = hashable_key(idty)
-            this_idty = by_idty.setdefault(idkey, [])
-            for contact in doc['contacts']:
-                contact_id = contact[0]
-                this_idty.append(contact_id)
-                by_contact.setdefault(contact_id, []).append(idty)
-        # part 2
-        found = []
-        for idid in idids:
-            if idid in by_idty:
-                found.append(idid)
-        return found
+            all_known.add(idkey)
+        # now return the union.
+        return all_known.intersection(set(idids))
 
     def _fetch_messages(self, db, msg_keys):
         # Generate proper key for megaview lookup.
@@ -146,7 +160,7 @@ class ConversationAPI(API):
         # TODO: this should probably be a back-end extension.
         # TODO: as above, fix the above comment :)
 
-        idtys = self._fetch_contact_identities(db, from_map)
+        idtys = self._filter_known_identities(db, from_map)
         # Cycle through the identities, and work up a schema for
         # them if they are known.
         for idty in idtys:
@@ -194,27 +208,24 @@ class ConversationAPI(API):
     def home(self, req):
         self.requires_get(req)
         args = self.get_args(req, limit=30)
-        db = RDCouchDB(req['path'][0]) # first elt of path is db name
+        db = RDCouchDB(req)
 
         result = db.megaview(startkey=["rd.msg.recip-target", "target-timestamp", ['direct', {}]],
                              endkey=["rd.msg.recip-target", "target-timestamp", ['direct']],
                              descending=True,
                              reduce=False,
-                             include_docs=True,
+                             include_docs=False,
                              limit=args['limit'])
 
+        # note the results are already sorted by descending timestamp
         # Filter out the items that are not "fresh".
         all_recips = self._filter_fresh(db,
-                                        [row['doc'] for row in result['rows']
-                                         if 'doc' in row])
-        # sort the allRecips by timestamp
-        all_recips.sort(key=lambda d: d['timestamp'], reverse=True)
-
+                                        [row['value']['rd_key'] for row in result['rows']])
         # The json has the rd_key for messages in timestamp order;
         # now we need to fetch the 'rd.msg.conversation' schema to fetch the
         # convo ID.
-        keys = [['rd.core.content', 'key-schema_id', [doc['rd_key'], 'rd.msg.conversation']]
-                for doc in all_recips]
+        keys = [['rd.core.content', 'key-schema_id', [rdkey, 'rd.msg.conversation']]
+                for rdkey in all_recips]
         result = db.megaview(keys=keys,
                              reduce=False,
                              include_docs=True)
@@ -227,7 +238,75 @@ class ConversationAPI(API):
         convos = self._fetch_conversations(db, conv_ids)
         # sort based on timestamp on latest message in convo.
         convos.sort(key=lambda c: c[-1]["rd.msg.body"]["timestamp"], reverse=True)
-        return make_response(convos)
+        return convos
+
+    # Fetch all conversations which have the specified messages in them.
+    def with_messages(self, req):
+        self.requires_get_or_post(req)
+        args = self.get_args(req, 'keys')
+        db = RDCouchDB(req)
+        return self._with_messages(db, args['keys'])
+
+    def _with_messages(self, db, msg_keys):
+        keys = [['rd.core.content', 'key-schema_id', [k, 'rd.msg.conversation']]
+                for k in msg_keys]
+
+        result = db.megaview(keys=keys, reduce=False, include_docs=True)
+
+        conv_set = set()
+        for row in result['rows']:
+            conv_set.add(row['doc']['conversation_id'])
+        conv_ids = list(conv_set)
+
+        # now fetch the conversation objects.
+        convos = self._fetch_conversations(db, conv_ids)
+        # and no sorting for this function.
+        return convos
+
+    # Fetch all conversations which include a message from the specified contact
+    def contact(self, req):
+        self.requires_get(req)
+        args = self.get_args(req, "id")
+        cid = args['id']
+
+        db = RDCouchDB(req)
+        capi = ContactAPI()
+        idids = capi._fetch_identies_for_contact(db, cid)
+        return _identities(db, idids)
+
+    # Fetch all conversations which include a message from the specified contact
+    def identities(self, req):
+        # fetch messages 'from' any of those identities
+        # XXX - shouldn't we do 'to' etc too?
+        db = RDCouchDB(req)
+        self.requires_get(req)
+        args = self.get_args(req, ids=None)
+        ids = args['ids']
+        if ids is None:
+            # special case - means "my identity".  Note this duplicates code
+            # in raindrop/extenv.py's get_my_identities() function.
+            result = db.megaview(startkey=["rd.account", "identities"],
+                                 endkey=["rd.account", "identities", {}],
+                                 reduce=False)
+            mine = set()
+            for row in result['rows']:
+                iid = row['key'][2]
+                mine.add(hashable_key(iid))
+            ids = list(mine)
+        return self._identities(db, ids)
+
+    def _identities(self, db, idids):
+        keys = [["rd.msg.body", "from", idid] for idid in idids]
+        keys.extend([["rd.msg.body", "to", idid] for idid in idids])
+        keys.extend([["rd.msg.body", "cc", idid] for idid in idids])
+        result = db.megaview(keys=keys, reduce=False)
+        message_keys = [r['value']['rd_key'] for r in result['rows']]
+        # Load the conversations based on these message IDs.
+        result = self._with_messages(db, message_keys)
+        # sort the result
+        result.sort(key=lambda c: c[0]['rd.msg.body']['timestamp'],
+                    reverse=True)
+        return result
 
     # Helper for most other end-points
     def _query(self, req, **kw):
@@ -245,7 +324,7 @@ class ConversationAPI(API):
         args = self.get_args(req, limit=30, skip=0)
         opts['limit'] = args['limit']
         opts['skip'] = args['skip']
-        db = RDCouchDB(req['path'][0]) # first elt of path is db name
+        db = RDCouchDB(req)
 
         result = db.megaview(**opts)
 
@@ -265,7 +344,7 @@ class ConversationAPI(API):
         convos = self._fetch_conversations(db, conv_ids)
         # sort based on timestamp on latest message in convo.
         convos.sort(key=lambda c: c[-1]["rd.msg.body"]["timestamp"], reverse=True)
-        return make_response(convos)
+        return convos
 
     # The 'simpler' end-points based around self._query()
     def direct(self, req):
@@ -285,6 +364,25 @@ class ConversationAPI(API):
                            startkey=["rd.msg.recip-target", "target-timestamp", ["broadcast", {}]],
                            endkey=["rd.msg.recip-target", "target-timestamp", ["broadcast"]],
                            )
+
+
+class ContactAPI(API):
+    def _fetch_identies_for_contact(self, db, cid):
+        # find all identity-ids for the contact
+        startkey = ['rd.identity.contacts', 'contacts', [cid]]
+        endkey = ['rd.identity.contacts', 'contacts', [cid, {}]]
+        result = db.megaview(startkey=startkey, endkey=endkey, reduce=False)
+        ret = []
+        for row in result['rows']:
+            # the rd_key should be ['identity', idid]
+            rdkey = row['value']['rd_key']
+            if (rdkey[0] == 'identity'):
+                assert len(rdkey)==2
+                ret.append(rdkey[1])
+            else:
+                log("contact has non-identity record: %s", row)
+        return ret
+
 
 # A mapping of the 'classes' we expose.  Each value is a class instance, and
 # all 'public' methods (ie, those without leading underscores) on the instance
@@ -310,7 +408,7 @@ def handler(request):
         if not callable(meth) or meth_name.startswith('_'):
             raise APIErrorResponse(400, "invalid API request endpoint function")
         # phew - call it.
-        resp = meth(request)
+        resp = make_response(meth(request))
     except APIException, exc:
         resp = exc.err_response
     except Exception, exc:
