072c873fd4b0b2f8538bcc2694882845831bb1d5
Andy Chung <andy@mozillamessaging.com>
2009-12-14 17:53:47 -0800

    minor changes

--- client/lib/rdw/nls/i18n.js
+++ client/lib/rdw/nls/i18n.js
@@ -46,7 +46,7 @@
   messagePlural: "messages",
   more: "more",
   moreMessages: "${count} more ${messagePlural}...",
-  newCount: "${count} new",
+  newCount: "${count} new from",
   poundCount: "(${count})",
   recent: "Recent searches:",
   reply: "reply",
