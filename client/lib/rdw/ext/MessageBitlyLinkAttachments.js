b4611721f7a43210cbad53641980ce1c5702efc3
Bryan Clark <clarkbw@gnome.org>
2010-01-08 14:04:02 -0800

    do an initial bit.ly link display; this isn't near finalized but i'm leaving that up to Andy to take a look at

---
+++ client/lib/rdw/ext/MessageBitlyLinkAttachments.js
@@ -0,0 +1,71 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is Raindrop.
+ *
+ * The Initial Developer of the Original Code is
+ * Mozilla Messaging, Inc..
+ * Portions created by the Initial Developer are Copyright (C) 2009
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ * */
+
+dojo.provide("rdw.ext.MessageBitlyLinkAttachments");
+
+dojo.require("rdw.Message");
+
+/*
+Applies a display extension to rdw.Message.
+Allows showing links included in the message as inline attachments
+*/
+
+rd.applyExtension("rdw.ext.MessageBitlyLinkAttachments", "rdw.Message", {
+  after: {
+    postCreate: function() {
+      //NOTE: the "this" in this function is the instance of rdw.Message.
+
+      //Check for links found in a message
+      var bitly_schema = this.msg.schemas["rd.msg.body.bit.ly"];
+      if (!bitly_schema ) {
+        return;
+      }
+
+      href = "href=\"" + bitly_schema["longUrl"] + "\""
+      title = "<a target=\"_blank\" class=\"title\" " + href + "\">" +
+                bitly_schema["htmlTitle"] + "</a>";
+      owner = "<abbr class=\"owner\">" + bitly_schema["shortenedByUser"] + "</abbr>";
+      desc = "<div class=\"description\">" + bitly_schema["longUrl"] + "</div>";
+
+      //Create a node to hold the link object
+      var linkNode = dojo.create("div", {
+        "class": "bitly link",
+        innerHTML: title + owner + desc
+      });
+      dojo.query(".message .attachments", this.domNode).addContent(linkNode);
+      dojo.connect(linkNode, "onclick", this, "onMessageBitlyLinkAttachmentClick");
+
+    }
+  },
+  addToPrototype: {
+    onMessageBitlyLinkAttachmentClick: function(evt) {
+      //summary: handles clicking anywhere on the link attachment block
+      var link_schema = this.msg.schemas["rd.msg.body.bit.ly"];
+      if (!link_schema ) {
+        return;
+      }
+    }
+  }
+});
+
+rd.addStyle("rdw.ext.css.MessageBitlyLinkAttachments");
